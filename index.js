b5e5b0342ed906d923cb95724a10f6af64bb7cb6
dankbjss <dan.kiernan@bjss.com>
2024-08-12 10:31:52 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+/**
+ * Adds a global event listener to the specified parent element.
+ * This is lifted from the Web Dev Simplified YouTube channel.
+ *  
+ * @link https://www.youtube.com/watch?v=cOoP8-NPLSo
+ *
+ * @param {string} type - The type of event to listen for.
+ * @param {string} selector - The CSS selector for the target element(s).
+ * @param {function} callback - The callback function to be executed when the event is triggered.
+ * @param {HTMLElement} [parent=document] - The parent element to which the event listener is attatched.
+ */
+function addGlobalEventListner(type, selector, callback, parent = document) {
+    parent.addEventListener(type, (event) => {
+        if (event.target.matches(selector)) {
+            callback(event);
+        }
+    });
+};
+
+addGlobalEventListner('click', '#open-dialog-button', (e) => {
+    const modalDialog = document.getElementById('modal-dialog');
+    modalDialog.showModal();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleCheckbox = document.getElementById('switch');
+    const currentTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    themeToggleCheckbox.checked = currentTheme === 'dark';
+
+    addGlobalEventListner('change', '#switch', () => {
+        const newTheme = themeToggleCheckbox.checked ? 'dark' : 'light';
+        document.documentElement.setAttribute('data-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
+    });
+});
